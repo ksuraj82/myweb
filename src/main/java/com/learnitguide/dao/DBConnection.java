bd947b3ede02a34d2a3595ca9e9bb0e6a3b66a14
Suraj kumar <37185827+ksuraj82@users.noreply.github.com>
2026-06-15 02:19:02 +0530

    fixed the user and password parameter

--- src/main/java/com/learnitguide/dao/DBConnection.java
+++ src/main/java/com/learnitguide/dao/DBConnection.java
@@ -16,7 +16,7 @@ public class DBConnection {
 	
  private static final String JDBC_URL = "jdbc:postgresql://"+System.getProperty("DB_HOST")+":"+System.getProperty("DB_PORT")+"/"+System.getProperty("DB_NAME"); // your DB name
  private static final String JDBC_USER = System.getProperty("DB_USER"); // your DB username
- private static final String JDBC_PASS = System.getProperty("DB_USER"); // your DB password
+ private static final String JDBC_PASS = System.getProperty("DB_PASS"); // your DB password
 
  // Method to get a connection
  
